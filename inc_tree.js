6d283221d31b46a522e5e69c13c8df2c97bbcf91
kouzi <kouzi@ktime.cc>
2016-09-02 18:31:52 +0800

    v-0.3

--- inc_tree.js
+++ inc_tree.js
@@ -25,27 +25,80 @@ function www(e,canvas,info){
 //----------------------------------------------------------------
 
 
-function cell(o){
-    this.left = 1;
-    this.right = 2;
-    this.show = function(){
-        for(var i=0;i<4;i++){
-            this.push(o);
+function cell(arr,size_cell_info){
+    //the cell start&end position
+    this.pos_all=[0,0,0,0];
+    this.pos_next = [];
+    this.angle;
+    this.child_len = rand(0,4);
+    this.ccc = [];
+    for(var i = 0;i<this.child_len;i++){
+        this.ccc[i]=null;
+        buildInfo(this,arr,size_cell_info);
+    }
+}
+function cau(){
+    this.pos_all[2] = rand(this.pos_all[0],0);
+}
+
+
+//return Array contains new position of Cell
+function buildInfo (obj,arr,size_cell_info){
+    //new position
+
+    var ang = (arr[2]+Math.PI/2)/2;
+    var angle_last = rand_float(ang-size_cell_info[2]/2,size_cell_info[2]);
+    var size = rand(size_cell_info[0],size_cell_info[1]);
+    var x =size*Math.cos(angle_last);
+    var y =size*Math.sin(angle_last);
+
+    //set positon of cell
+    this.pos_all[0]=arr[0];
+    this.pos_all[1]=arr[1];
+    this.pos_all[2]=x+arr[0];
+    this.pos_all[3]=y+arr[1];
+
+}
+
+function TREE_MAIN(){
+    this.root = null;
+    this.build = build;
+}
+function build(size_cell_info){
+    if(this.root == null){
+        //start_pos(x,y),length_strength(start,range)
+        arr = [540,300];
+        var n = new cell(arr,size_cell_info);
+        this.root = n;
+    }else{
+        var current = this.root;
+        var parent;
+        while(true){
+            parent = current;
+            for(var i=0;i<parent.child_len;i++){
+                current = current.ccc[i];
+                //build arr info
+                var arr =[parent.pos_next[i][0],parent.pos_next[i][1],parent.angle];
+                n = new cell(arr,size_cell_info);
+                if(current== null){
+                    parent.ccc[i] =n;
+                    break;
+                }
+            }
         }
-        return this;
     }
 }
 
-//var o = new Object();
-//
-//console.log(new cell(o).show());
-
-
+var angle_range = Math.PI/6;
+var size_cell_info = [30,60,angle_range];
 //--------------------------------------------------------------
 
 
 function rand(start,range){
     return Math.round(Math.random()*range+start);
+}
+function rand_float(start,range){
+    return Math.random()*range+start;
 }
 
 function randNum(start,range,num){
