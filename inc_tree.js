c0cee3da23b8c168130e62a51b5261cd9b863259
ElonKou <kouzi@ktime.cc>
2016-09-16 19:06:36 +0800

    v1.0,succeed

--- inc_tree.js
+++ inc_tree.js
@@ -25,17 +25,17 @@ function www(e,canvas,info){
 //----------------------------------------------------------------
 
 var id = 0;
-function cell(arr,size_cell_info){
+function cell(arr,size_cell_info,lineWidth,num){
     //the cell start&end position
     this.id = id;
     id++;
     this.pos_all=[];
     this.pos_next = [];
     //this.angle = null;
-    this.child_len = rand(1,3);
+    this.child_len = rand(1,num);
     this.ccc = [];
 
-    var ang = (arr[2]+parseInt(Math.PI/2))/2;
+    var ang = (parseFloat(arr[2])+1.5708)/2;
 
     var angle_last = rand_float(ang-size_cell_info[2]/2,size_cell_info[2]);
     var size = rand(size_cell_info[0],size_cell_info[1]);
@@ -45,7 +45,7 @@ function cell(arr,size_cell_info){
     this.pos_all[0]=arr[0];
     this.pos_all[1]=arr[1];
     this.pos_all[2]=x+arr[0];
-    this.pos_all[3]=y+arr[1];
+    this.pos_all[3]=arr[1]-y;
     this.angle = angle_last.toFixed(2);
 
 
@@ -55,9 +55,11 @@ function cell(arr,size_cell_info){
         var ratio = rand_float(size_cell_info[3],size_cell_info[4]);
         var x_x = Math.round(size * ratio * Math.cos(angle_last));
         var y_y = Math.round(size * ratio * Math.sin(angle_last));
-        this.pos_next.push([x_x+arr[3],y_y+arr[4]]);
+        this.pos_next.push([x_x+arr[0],arr[1]-y_y]);
         //-------------------
     }
+    draw(this.pos_all,lineWidth);
+    drawT(this.pos_all[2],this.pos_all[3],lineWidth+3);
 }
 
 
@@ -65,13 +67,12 @@ function cell(arr,size_cell_info){
 function TREE_MAIN(){
     this.root = null;
     this.build = build;
-    //this.inOrder = inOrder;
 }
 //foreach
 var time = 0;
-function inOrder(node){
+function inOrder(node,lineWidth,num){
     //---------------------------------------------------------------------------------
-    if(time>=40){
+    if(time>=500){
         return false;
     }
     time ++;
@@ -82,58 +83,98 @@ function inOrder(node){
     if(parent != null){
         for(var i = 0; i < parent.child_len;i++){
             current = parent.ccc[i];
-            console.log("par_id:"+parent.id);
             if(current == null){
-                console.log(parent);
                 var ar =[parent.pos_next[i][0],parent.pos_next[i][1],parent.angle,parent.pos_all[0],parent.pos_all[1]];
-                console.log(ar);
-                var no = new cell(ar,size_cell_info);
+                //console.log(ar);
+                var no = new cell(ar,size_cell_info,lineWidth,num);
                 parent.ccc[i] = no;
-                //current = no;
-                //var n = new cell([33,34,Math.PI/2,99,99],size_cell_info);
-                //parent.ccc[i] = n;
             }else{
-                parent = current;
-                inOrder(current);
+                inOrder(current,lineWidth,num);
             }
         }
     }
 
 }
 
-function build(size_cell_info){
+
+function build(size_cell_info,lineWidth,num){
     if(this.root == null){
-        var arr = [540,300,Math.PI/2,540,340];
-        var n =  new cell(arr,size_cell_info);
+        var arr = [can_wid/2,can_hei,Math.PI/2,can_wid/2,can_hei];
+        var n =  new cell(arr,size_cell_info,lineWidth,num);
         this.root = n;
     }else{
         var node = this.root;
-        console.log("---------------------------");
-        inOrder(node);
+        inOrder(node,lineWidth,num);
     }
 }
 
-var size_start = 30;
-var size_end = 60;
-var angle_range = Math.PI/6;
-var size_pos_start = 0.8;
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var can_wid = canvas.width;
+var can_hei = canvas.height;
+
+function draw(arr,ww){
+
+    ctx.beginPath();
+
+    ctx.strokeStyle = radgrad_2;
+    ctx.lineCap = 'round';
+    ctx.lineWidth = ww;
+    ctx.moveTo(arr[0],arr[1]);
+    ctx.lineTo(arr[2],arr[3]);
+    ctx.stroke();
+}
+
+function drawT(x,y,r){
+    ctx.beginPath();
+
+    ctx.fillStyle = radgrad_1;
+    ctx.moveTo(x,y);
+    ctx.quadraticCurveTo(x-r,y-r,x-r,y-2*r);
+    ctx.quadraticCurveTo(x-r,y-3*r,x,y-3*r);
+    ctx.quadraticCurveTo(x+r,y-3*r,x+r,y-2*r);
+    ctx.quadraticCurveTo(x+r,y-r,x,y);
+
+    ctx.fill();
+}
+
+
+var size_start = 50;
+var size_end = 150;
+var angle_range = Math.PI/2;
+var size_pos_start = 0.7;
 var size_pos_end= 0.2;
 var size_cell_info = [size_start,size_end,angle_range,size_pos_start,size_pos_end];
 
 var tr = new TREE_MAIN();
 
-tr.build(size_cell_info);
-//console.log(tr);
-//tr.build(40,50,Math.PI/6,0.7,0.3);
-//tr.build(30,60,Math.PI/6,0.8,0.2);
-//tr.build(30,60,Math.PI/6,0.8,0.2);
-tr.build(30,60,Math.PI/6,0.8,0.2);
-tr.build(30,60,Math.PI/6,0.8,0.2);
-tr.build(30,60,Math.PI/6,0.8,0.2);
+var color_1 = "rgba("+(25*rand(0,10))+","+(25*rand(0,10))+","+(25*rand(0,10))+",1)";
+var color_2 = "rgba("+(25*rand(0,10))+","+(25*rand(0,10))+","+(25*rand(0,10))+",0.8)";
+//渐变
+var radgrad_1 = randRad();
+var radgrad_2 = randRad();
+
+
+
+tr.build(size_cell_info,10,3);
+tr.build(size_cell_info,8,3);
+tr.build(size_cell_info,6,3);
+tr.build(size_cell_info,5,3);
+tr.build(size_cell_info,3,4);
+tr.build(size_cell_info,1.2,2);
+tr.build(size_cell_info,1,1);
 console.log(tr);
 //---------------------------------------------------------------
 
+function randRad(){
+    var rad = ctx.createRadialGradient(can_wid/2,can_hei,1,can_wid/2,can_hei,1200);
+    rad.addColorStop(0,"rgba("+rand(0,255)+","+rand(0,255)+","+rand(0,255)+",0.8)");
+    rad.addColorStop(0.5,"rgba("+rand(0,255)+","+rand(0,255)+","+rand(0,255)+",0.8)");
+    rad.addColorStop(1,"rgba("+rand(0,255)+","+rand(0,255)+","+rand(0,255)+",0.8)");
 
+    console.log("de");
+    return rad;
+}
 
 
 
